957bc1652db6ad166db4ae39a26f2c780c0fba85
Benson <bensonwlee@gmail.com>
2023-07-05 16:51:14 -0400

    added movement keys to input.js

--- input.js
+++ input.js
@@ -1,11 +1,9 @@
-// event listener function if input is ctrl + c, exit
-const handleUserInput = function (data) {
-  if (data === "\u0003");
-  process.exit();
-};
+// Stores the active TCP connection object.
+let connection;
 
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,6 +11,23 @@ const setupInput = function () {
   // if input is ctrl + c, exit
   stdin.on("data", handleUserInput);
   return stdin;
+};
+
+// event listener function
+const handleUserInput = function (data) {
+  // if input is ctrl + c, exit
+  if (data === "\u0003") {
+    process.exit();
+    // moves for snake
+  } else if (data === "w") {
+    connection.write("Move: up");
+  } else if (data === "a") {
+    connection.write("Move: left");
+  } else if (data === "s") {
+    connection.write("Move: down");
+  } else if (data === "d") {
+    connection.write("Move: right");
+  };
 };
 
 module.exports = { setupInput };
--- play.js
+++ play.js
@@ -2,6 +2,9 @@ const { connect } = require("./client");
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+const connection = connect();
+
+setupInput(connection);
+
+module.exports = connection;
