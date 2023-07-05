04f55baddc724b49bd4eaea870748308a2e5c62f
Benson <bensonwlee@gmail.com>
2023-07-05 15:11:09 -0400

    added connection message and user name

--- client.js
+++ client.js
@@ -1,10 +1,28 @@
 const net = require("net");
+const userName = "BEN";
+
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: 50541
   });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // added // listen for and receive data from server
+  conn.on("data", data => {
+    console.log(data);
+  })
+  // added // message on connecting to server
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    // added // send user name
+    conn.write(`${userName}`);
+  })
+
+  return conn;
 };
 
 module.exports = { connect };
--- play.js
+++ play.js
@@ -1,14 +1,5 @@
+const net = require("net");
 const { connect } = require("./client");
-
-// // interpret incoming data as text
-// conn.setEncoding("utf8");
-
-// // added // listen for and receive data from server
-// conn.on("data", data => {
-//   console.log(data);
-// });
-
-// return conn;
 
 console.log("Connecting ...");
 connect();
